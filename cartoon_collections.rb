dc225e73beed88283364d2d24ff6c52a5482b695
manon jacquin <manon.jacquin@gmail.com>
2019-02-21 16:34:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,6 +22,7 @@ end
 def find_the_cheese(ingredients)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  # return the first element evaluating true inside the block
   cheese_types.find do |item|
     ingredients.include?(item)
   end
